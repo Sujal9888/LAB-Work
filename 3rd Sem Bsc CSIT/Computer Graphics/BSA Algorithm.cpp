0635ff5ac3e44d4b6c2764fc5cc26c930ff09116
Sujal Meher Kayastha <csit2081001_sujal@achsnepal.edu.np>
2025-12-20 19:34:13 +0545

    Done

--- 3rd Sem Bsc CSIT/Computer Graphics/BSA Algorithm.cpp
+++ 3rd Sem Bsc CSIT/Computer Graphics/BSA Algorithm.cpp
@@ -1,82 +1,68 @@
 #include <iostream>
-#include<graphics.h>
-#include<conio.h>
+#include <graphics.h>
+#include <conio.h>
+#include <cmath>
 
 using namespace std;
 
 int main() {
-	
-	int x1,x2,y1,y2;
-	cout<<"Enter (x1,y1): ";
-	cin>>x1>>y1;
-	cout<<"Enter (x2,y2): ";
-	cin>>x2>>y2;
-	
-	if(x1>x2){
-		int temp = x1;
-		x1 = x2;
-		x2 = temp;
-	}
-	if( y1>y2){
-		int temp = y1;
-		y1 = y2;
-		y2 = temp;
-	}
-	int gd = DETECT,gm;
-	
-	initgraph(&gd,&gm,"");
-	
-	int dx = x2-x1;
-	int dy = y2-y1;
-	
-	int Xin = x1;
-	int Yin = y1;
-	int p;
-	
-	if(dy>=dx){
-		p = 2 * dx - dy;
-	
-	putpixel(Xin,Yin,WHITE);
-	}
-	else if(dx>dy){
-		 p = 2 * dy - dx;
-	
-	putpixel(Xin,Yin,WHITE);
-	}
-	
-	int k=0;
-	cout<<"K   	 Pk     Xk+1 Yk+1"<<endl;
-	if(dx>dy){
-		while(Xin < x2){
-			if(p<0){
-				p = p + 2 * dy;
-			}
-			else{
-				Yin++;
-				p = p + 2*(dy-dx);
-			}
-			Xin++;
-			cout<<k<<"\t"<<p<<" \t( "<<Xin<<","<<Yin<<")\n";
-			k++;
-			putpixel(Xin,Yin,WHITE);
-		}
-	}
-	if(dy>=dx){
-		while(Yin<y2){
-			if(p<0){
-				p = p+2*dx;
-			}
-			else{
-				Xin++;
-				p = p + 2 * (dx-dy);
-			}
-			Yin++;
-			cout<<k<<"\t"<<p<<"\t("<<Xin<<","<<Yin<<")\n";
-			k++;
-			putpixel(Xin,Yin,WHITE);
-		}
-	}
-	getch();
-	closegraph();
-	return 0;
+    int x1, y1, x2, y2;
+    cout << "Enter (x1,y1): ";
+    cin >> x1 >> y1;
+    cout << "Enter (x2,y2): ";
+    cin >> x2 >> y2;
+
+    int dx = abs(x2 - x1);
+    int dy = abs(y2 - y1);
+
+    int Xin = x1, Yin = y1;
+
+    // Determine step directions
+    int sx = (x2 >= x1) ? 1 : -1;
+    int sy = (y2 >= y1) ? 1 : -1;
+
+    int p;
+    int gd = DETECT, gm;
+    initgraph(&gd, &gm, "");
+
+    int k = 0;
+    cout << "K\tPk\t(Xk+1,Yk+1)" << endl;
+
+    if (dx > dy) { // Slope < 1
+        p = 2 * dy - dx;
+        putpixel(Xin, Yin, WHITE);
+
+        while (Xin != x2) {
+            Xin += sx;
+            if (p < 0) {
+                p += 2 * dy;
+            } else {
+                Yin += sy;
+                p += 2 * (dy - dx);
+            }
+            cout << k << "\t" << p << "\t(" << Xin << "," << Yin << ")" << endl;
+            putpixel(Xin, Yin, WHITE);
+            k++;
+        }
+    } else { // Slope >= 1
+        p = 2 * dx - dy;
+        putpixel(Xin, Yin, WHITE);
+
+        while (Yin != y2) {
+            Yin += sy;
+            if (p < 0) {
+                p += 2 * dx;
+            } else {
+                Xin += sx;
+                p += 2 * (dx - dy);
+            }
+            cout << k << "\t" << p << "\t(" << Xin << "," << Yin << ")" << endl;
+            putpixel(Xin, Yin, WHITE);
+            k++;
+        }
+    }
+
+    getch();
+    closegraph();
+    return 0;
 }
