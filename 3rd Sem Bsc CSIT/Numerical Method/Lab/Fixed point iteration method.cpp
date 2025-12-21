be35208caec318759b282d00c34e24af4562bad5
Sujal Meher Kayastha <csit2081001_sujal@achsnepal.edu.np>
2025-12-21 17:24:37 +0545

    Done

--- 3rd Sem Bsc CSIT/Numerical Method/Lab/Fixed point iteration method.cpp
+++ 3rd Sem Bsc CSIT/Numerical Method/Lab/Fixed point iteration method.cpp
@@ -12,21 +12,21 @@ int main(){
     printf("\nEnter initial guess x0: ");
     scanf("%lf", &x0);
 
-    printf("\nFixed Point Iteration for x = (x^2 + 2)/3\n");
+    printf("\nFixed Point Iteration for x^3 + x^2 - 3x - 3 = 0\n");
     printf("\n%-6s %-15s %-15s\n", "Iter", "Xn", "Xn+1");
     printf("------------------------------------------------\n");
 
-    do{
+    while(1){
         itr++;
         x1 = g(x0);
+
         printf("%-6d %-15.6lf %-15.6lf\n", itr, x0, x1);
 
         if(fabs(x1 - x0) <= 0.0001) break;
         x0 = x1;
-    } while(1);
+    }
 
-    printf("\nRoot found at  %.6lf\n", x1);
+    printf("\nRoot found at %.6lf\n", x1);
 
     return 0;
 }
-
