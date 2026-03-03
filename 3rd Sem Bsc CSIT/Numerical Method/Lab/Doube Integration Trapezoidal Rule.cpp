eb3511cefde7f3f96f3356828b2c6c5166e2e7a8
Sujal Meher Kayastha <sujalid2468@gmail.com>
2026-03-03 17:05:18 +0545

    Lab 3

--- 3rd Sem Bsc CSIT/Numerical Method/Lab/Doube Integration Trapezoidal Rule.cpp
+++ 3rd Sem Bsc CSIT/Numerical Method/Lab/Doube Integration Trapezoidal Rule.cpp
@@ -3,36 +3,52 @@
 
 float f(float x, float y)
 {
-    return x*y;
+    return x * y;   // Change function if needed
 }
 
 int main()
 {
-    float a,b,c,d,hx,hy,sum=0;
-    int nx,ny,i,j;
-	printf("---Double Integration Trapezoidal Rule---\n ");
+    float a, b, c, d, hx, hy, sum = 0;
+    int nx, ny, i, j;
+
+    printf("--- Double Integration using Trapezoidal Rule ---\n");
+
     printf("Enter x-lower and x-upper: ");
-    scanf("%f %f",&a,&b);
+    scanf("%f %f", &a, &b);
+
     printf("Enter y-lower and y-upper: ");
-    scanf("%f %f",&c,&d);
-    printf("Enter nx and ny: ");
-    scanf("%d %d",&nx,&ny);
+    scanf("%f %f", &c, &d);
 
-    hx=(b-a)/nx;
-    hy=(d-c)/ny;
+    printf("Enter number of subintervals nx and ny: ");
+    scanf("%d %d", &nx, &ny);
 
-    for(i=0;i<=nx;i++)
+    hx = (b - a) / nx;
+    hy = (d - c) / ny;
+
+    for(i = 0; i <= nx; i++)
     {
-        for(j=0;j<=ny;j++)
+        for(j = 0; j <= ny; j++)
         {
-            float x=a+i*hx;
-            float y=c+j*hy;
-            sum+=f(x,y);
+            float x = a + i * hx;
+            float y = c + j * hy;
+
+            // Corner points
+            if((i == 0 || i == nx) && (j == 0 || j == ny))
+                sum += f(x, y);
+
+            // Edge points
+            else if(i == 0 || i == nx || j == 0 || j == ny)
+                sum += 2 * f(x, y);
+
+            // Interior points
+            else
+                sum += 4 * f(x, y);
         }
     }
 
-    printf("Double Integration = %f",sum*hx*hy);
+    sum = (hx * hy / 4) * sum;
+
+    printf("Double Integration = %f\n", sum);
 
     return 0;
 }
-
