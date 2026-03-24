dbbcc5db62acea24130e1f79ad87f8b173020484
Sujal Meher Kayastha <sujalid2468@gmail.com>
2026-03-24 17:07:17 +0545

    Fixed

--- 3rd Sem Bsc CSIT/Data Sturcture and Algorithm/Lab/Minimum spanning tree.cpp
+++ 3rd Sem Bsc CSIT/Data Sturcture and Algorithm/Lab/Minimum spanning tree.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <climits>
 using namespace std;
 
 /* ----------- Kruskal's Algorithm ----------- */
@@ -8,49 +9,61 @@ struct Edge {
     int u, v, weight;
 };
 
-int findParent(int parent[], int i) {
+// Comparator function
+bool compareEdges(const Edge &a, const Edge &b) {
+    return a.weight < b.weight;
+}
+
+int findParent(vector<int>& parent, int i) {
     if (parent[i] == i)
         return i;
     return parent[i] = findParent(parent, parent[i]);
 }
 
-void unionSets(int parent[], int u, int v) {
-    parent[findParent(parent, u)] = findParent(parent, v);
+void unionSets(vector<int>& parent, int u, int v) {
+    int pu = findParent(parent, u);
+    int pv = findParent(parent, v);
+    parent[pu] = pv;
 }
 
 void Kruskal(vector<Edge> edges, int n) {
-    sort(edges.begin(), edges.end(), [](Edge a, Edge b){ return a.weight < b.weight; });
-    int parent[n];
-    for(int i = 0; i < n; i++) parent[i] = i;
+    sort(edges.begin(), edges.end(), compareEdges);
+
+    vector<int> parent(n);
+    for(int i = 0; i < n; i++)
+        parent[i] = i;
 
     cout << "\nKruskal's MST edges:\n";
     int total = 0;
-    for(auto e : edges){
+
+    for(size_t i = 0; i < edges.size(); i++){
+        Edge e = edges[i];
+
         int u = findParent(parent, e.u);
         int v = findParent(parent, e.v);
+
         if(u != v){
             cout << e.u << " - " << e.v << " : " << e.weight << "\n";
             total += e.weight;
             unionSets(parent, u, v);
         }
     }
+
     cout << "Total weight of MST: " << total << "\n";
 }
 
 /* ----------- Prim's Algorithm ----------- */
-void Prim(int graph[10][10], int n) {
-    int key[10], parent[10];
-    bool mstSet[10] = {false};
-
-    for(int i = 0; i < n; i++){
-        key[i] = INT_MAX;
-        parent[i] = -1;
-    }
+void Prim(const vector<vector<int> >& graph, int n) {
+    vector<int> key(n, INT_MAX);
+    vector<int> parent(n, -1);
+    vector<bool> mstSet(n, false);
 
     key[0] = 0;
 
     for(int count = 0; count < n - 1; count++){
-        int u = -1, minKey = INT_MAX;
+        int u = -1;
+        int minKey = INT_MAX;
+
         for(int i = 0; i < n; i++){
             if(!mstSet[i] && key[i] < minKey){
                 minKey = key[i];
@@ -58,7 +71,7 @@ void Prim(int graph[10][10], int n) {
             }
         }
 
-        if(u == -1) break; // disconnected graph
+        if(u == -1) break;
         mstSet[u] = true;
 
         for(int v = 0; v < n; v++){
@@ -71,30 +84,43 @@ void Prim(int graph[10][10], int n) {
 
     cout << "\nPrim's MST edges:\n";
     int total = 0;
+
     for(int i = 1; i < n; i++){
         if(parent[i] != -1){
             cout << parent[i] << " - " << i << " : " << graph[i][parent[i]] << "\n";
             total += graph[i][parent[i]];
         }
     }
+
     cout << "Total weight of MST: " << total << "\n";
 }
 
-/* ----------- Main Menu ----------- */
+/* ----------- Main Function ----------- */
 int main(){
     int choice, n;
-    int graph[10][10];
-    vector<Edge> edges;
 
-    cout << "Enter number of vertices (max 10): ";
+    cout << "Enter number of vertices: ";
     cin >> n;
+
+    if(n <= 0){
+        cout << "Invalid number of vertices!\n";
+        return 0;
+    }
+
+    vector<vector<int> > graph(n, vector<int>(n));
+    vector<Edge> edges;
 
     cout << "Enter adjacency matrix (0 if no edge):\n";
     for(int i = 0; i < n; i++){
         for(int j = 0; j < n; j++){
             cin >> graph[i][j];
+
             if(i < j && graph[i][j] != 0){
-                edges.push_back({i, j, graph[i][j]});
+                Edge e;
+                e.u = i;
+                e.v = j;
+                e.weight = graph[i][j];
+                edges.push_back(e);
             }
         }
     }
